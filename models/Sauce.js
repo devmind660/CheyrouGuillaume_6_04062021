a0d8440299edcec1b9096e59e1b89a0ba56a4c4f
Guillaume Cheyrou <devmind660@gmail.com>
2021-07-12 19:32:54 +0200

    Création des sauces

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-    id: { type: String, required: true },
     userId: { type: String, required: true },
-    title: { type: String, required: true },
+    name: { type: String, required: true },
     manufacturer: { type: String, required: true },
     description: { type: String, required: true },
     mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true },
-    likes: { type: Number, required: true },
-    dislikes: { type: Number, required: true },
-    userLiked: { type: String, required: true },
-    userDisliked: { type: String, required: true },
+    likes: { type: Number, required: true, default: 0},
+    dislikes: { type: Number, required: true, default: 0},
+    userLiked: { type: String, required: true, default: 0},
+    userDisliked: { type: String, required: true, default: 0},
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
